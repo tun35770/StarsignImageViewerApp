babcd4c0ee309ad9aa7a9092f9bf1957958061c2
tun35770 <tun35770@temple.edu>
2021-10-05 19:06:56 -0400

    Commit 1: Activities & layouts creation

---
+++ app/src/main/java/edu/temple/starsignimageviewerapp/SelectionActivity.kt
@@ -0,0 +1,14 @@
+package edu.temple.starsignimageviewerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
