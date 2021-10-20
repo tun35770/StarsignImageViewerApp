3effddc967e55360429d74d79396f5ac61e76803
tun35770 <tun35770@temple.edu>
2021-10-20 17:12:43 -0400

    Rename "Selection Activity" to "Main Activity", delete DisplayActivity.kt

--- app/src/main/java/edu/temple/starsignimageviewerapp/MainActivity.kt
+++ app/src/main/java/edu/temple/starsignimageviewerapp/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class SelectionActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
